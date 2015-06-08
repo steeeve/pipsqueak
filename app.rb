9e8d658c5037aac65f9f13ead48b1ca0838bc754
Stephen Mckellar <stephen@thingmaker.io>
2015-06-08 23:48:59 +0100

    Get pips

--- app.rb
+++ app.rb
@@ -7,3 +7,12 @@ post '/pip' do
 
   Pip.pip(label, value)
 end
+
+get '/pips' do
+  from = params['from']
+  to = params['to']
+
+  pips = Pip.find(from, to)
+
+  pips.to_json
+end
--- pip.rb
+++ pip.rb
@@ -1,7 +1,24 @@
 require 'date'
 require 'cassandra'
+require 'json'
 
 class Pip
+
+  attr_accessor :label, :time, :value
+
+  def initialize(params)
+    params.each do |key, value|
+      instance_variable_set("@#{key}", value) unless value.nil?
+    end
+  end
+
+  def to_json(*a)
+    {
+      label: label,
+      time: time,
+      value: value
+    }.to_json(*a)
+  end
 
   module ClassMethods
 
@@ -13,6 +30,23 @@ class Pip
       timestamp = now.to_time
       statement = session.prepare('INSERT INTO pips_by_day (label, date, time, value) VALUES (?, ?, ?, ?)')
       session.execute(statement, arguments: [label, date, timestamp, value])
+    end
+
+    def find(from_s, to_s)
+      from = DateTime.parse(from_s)
+      to = DateTime.parse(to_s)
+      from_time = from.to_time
+      to_time = to.to_time
+      statement = session.prepare('SELECT * ' \
+                                  'FROM pips_by_day ' \
+                                  'WHERE time > ? ' \
+                                  'AND time < ? ' \
+                                  'ALLOW FILTERING')
+      results = session.execute(statement, arguments: [from_time, to_time])
+
+      results.map do |row|
+        Pip.new(row)
+      end
     end
 
     private
